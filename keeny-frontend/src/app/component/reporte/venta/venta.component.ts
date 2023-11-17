6b21e4f4577fb021dbd51f96e455799605047518
Elmira_Becerra <elmira.miluzka@gmail.com>
2023-11-17 18:52:40 -0500

    Reporte de ventas - Exportar PDF (datos simulados)

--- keeny-frontend/src/app/component/reporte/venta/venta.component.ts
+++ keeny-frontend/src/app/component/reporte/venta/venta.component.ts
@@ -1,7 +1,8 @@
-import { state, style, trigger } from '@angular/animations';
+import { state, style, trigger,transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormGroup, Validator } from '@angular/forms';
 import { VentaService } from 'src/app/service/reporte/venta.service';
+
 
 @Component({
   selector: 'app-venta',
@@ -14,20 +15,31 @@ import { VentaService } from 'src/app/service/reporte/venta.service';
       })),
       state('hide',style({
         'max-height' : '0', 'opacity' : '0', 'visibilite' : 'hidden'
-      }))
+      })),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('300ms ease-in'))
     ])
   ]
 })
+
 export class VentaComponent implements OnInit{
-
-
+  //FrmVenta: FormGroup;
 
   constructor(
-    private srvReporte : VentaService
-  ) {}
+    private srvReporte : VentaService,
+    private srvImpresion: VentaService,
+    private fb: FormBuilder
+  ) {
+   /* this.FrmVenta = this.fb.group({
+      fecha: [''],
+      productoId: ['']
+    });*/
+  }
 
-  filtro : any;
- // FrmVenta: FormGroup;
+  filtro : any = {
+    fecha: '',
+    productoId: ''
+  };
   pagActual = 1;
   itemsPPag= 5;
   paginas =[2,5,10,20,50];
@@ -39,14 +51,34 @@ export class VentaComponent implements OnInit{
   onNuevo() { }
 
   onFiltrar() {
-    this.filtroVisible = !this.filtroVisible;
+   /* const estadoFiltro = this.stateFiltro === 'show' ? 'hice' : 'show';
+    this.FrmVenta.reset(); // Limpiar el frm
+    this.FrmVenta.get('fecha')?.setValue(''); // ?
+    this.FrmVenta.get('productoId')?.setValue('');
+    this.filtroVisible = estadoFiltro === 'show';*/
+   // this.filtroVisible = !this.filtroVisible;
+    this.Filtrar();
   }
   onImprimir() {
-
+    const encabezado = ["N° Boleta", "Fecha", "Id Cliente", "Id Usuario", "Total"]
+    const cuerpo = [
+                    ["001", "17/11/2023", "1", "2","50"],
+                    ["002", "15/10/2023", "2", "2","45.50"],
+                    ["003", "02/10/2023", "3", "2","12.70"]
+                  ]
+    this.srvImpresion.imprimir(encabezado, cuerpo, "Reporte de Ventas", true)
   }
   onCerrar() {
 
   }
+
+private Filtrar(): void {
+  const fecha = this.filtro.fecha;
+  const productoId = this.filtro.productoId;
+
+  console.log('Filtrar por Fecha:', fecha);
+  console.log('Filtrar por Producto ID:', productoId);
+}
 
   onCambioPag(e:any){
 
