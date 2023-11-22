295fb99a17c8ce1dc2aff6b6f51bc78056e5902c
nicolasal04 <nicoal2204@gmail.com>
2023-11-22 17:34:56 -0500

    AJUSTES DE NAV

--- keeny-frontend/src/app/component/inicio/inicio.component.ts
+++ keeny-frontend/src/app/component/inicio/inicio.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/service/mantenimiento/usuario/usuario.service';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/service/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
+
+
+  ngOnInit(): void {
+    console.log("Estoy Aqui");
+    this.obtenerNombreUsuario();
+  }
+
+  usuarioNombre = "Usuario";
+
+  constructor(private router: Router, private usuarioService : UsuarioService, private localStorageService : LocalStorageService){}
+
+  obtenerNombreUsuario() {
+    const email = this.localStorageService.obtenerEmail();
+    if(email){
+      this.usuarioService.obtenerUsuarioPorEmail(email).subscribe(data =>{
+        this.usuarioNombre = data.apellidoPaterno;
+      })
+    }
+  }
+
+  
   
 }
